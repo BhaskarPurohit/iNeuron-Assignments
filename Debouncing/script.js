381c4a6b95601e85ac11cc9e2ff1ea4e64b336e8
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-05-22 13:57:50 +0530

    222

---
+++ Debouncing/script.js
@@ -0,0 +1,8 @@
+//Bhaskar Purohit Debouncing example
+
+const input = document.querySelector("input")
+
+//log the message on input event
+input.addEventListener("input", ()=>{
+    console.log("Fetching data from database..");
+})
