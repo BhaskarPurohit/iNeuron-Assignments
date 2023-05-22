15fd8bb2da5ac8d17dd2ea66502b8d4de9d2e513
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-05-22 14:02:05 +0530

    2222

--- Debouncing/script.js
+++ Debouncing/script.js
@@ -1,8 +1,23 @@
 //Bhaskar Purohit Debouncing example
 
 const input = document.querySelector("input")
+let timeoutId;
 
-//log the message on input event
+const debounce = (callback)=>{
+    clearTimeout(timeoutId) //clear the previously set timeout
+
+    //execute callback after 1200 mili seconds
+    timeoutId = setTimeout(()=> callback(),1200)
+
+}
+
+//Register the debounce function on input event
 input.addEventListener("input", ()=>{
-    console.log("Fetching data from database..");
+    debounce(()=>{
+       console.log("Fetching data from database..");
+
+    })
+
+
+
 })
