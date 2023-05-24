f1aeba14ba79cf754dbbfffa365a333f5cfd9556
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-05-24 11:00:38 +0530

    24.2

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,6 +1,14 @@
-
+import {useState} from 'react'
 
 function App() {
+  const [calc, setCalc] = useState("")
+  const [result, setResult] = useState("")
+
+  const ops =  ['/','*','+','-','.']
+
+  const updateCalc = value =>{
+          setCalc(calc+value)
+  }
 
   const createDigits = () =>{
     const digits = []
@@ -15,7 +23,7 @@ function App() {
     <div className="App">
        <div className="calculator">
         <div className="display">
-          <span>(0)</span>
+          {result ? <span>(0)</span>: ''}{calc || '0'}
         </div>
         <div className="operators">
           <button>/</button>
